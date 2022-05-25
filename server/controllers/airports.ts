fdb49997a120aa180a72b5e8ec9d54b626be829e
ms-aija <aija.avota@gmail.com>
2022-05-25 17:26:06 +0200

    fix: add .d.ts file for amadeus library

--- server/controllers/airports.ts
+++ server/controllers/airports.ts
@@ -4,7 +4,7 @@ import Express from 'express';
 
 // No DB or models. Data coming directly from a json file.
 import airportsData from '../assets/airports.json';
-import { Airport } from '../interfaces/Airport';
+import { Airport } from '../types/Airport';
 
 export async function getAirportList(req: Express.Request, res: Express.Response) {
   try {
