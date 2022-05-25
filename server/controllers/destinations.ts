433a69191dbc52a5555c70d5c39ab08bd14e0ba2
ms-aija <aija.avota@gmail.com>
2022-05-25 18:02:46 +0200

    fix: add types to amadeus res in destination controller

--- server/controllers/destinations.ts
+++ server/controllers/destinations.ts
@@ -1,7 +1,9 @@
 // -- DESTINATIONS CONTROLLER
 // -- Destination data from Amadeus API
 import Amadeus from 'amadeus';
+import { AmadeusDestinationResult, AmadeusDestinationError } from 'amadeus'
 import Express from 'express';
+
 
 const amadeus = new Amadeus({
   clientId: process.env.CLIENT_ID,
@@ -11,14 +13,12 @@ const amadeus = new Amadeus({
 
 export function getDestinationCityList(req: Express.Request, res: Express.Response) {
   amadeus.client.get('/v1/airport/direct-destinations', { departureAirportCode: req.params.id, max: 300 })
-    .then(function (response) {
+    .then(function (response: AmadeusDestinationResult) {
       res.json(response.data);
       res.status(200);
     })
-    .catch(function (error) {
+    .catch(function (error: AmadeusDestinationError) {
       res.status(500);
       console.error(error);
     });
 }
-
-// module.exports = { getDestinationCityList };
--- server/types/amadeus/index.d.ts
+++ server/types/amadeus/index.d.ts
@@ -1,1 +1,37 @@
-declare module 'amadeus'
+declare module 'amadeus' {
+  interface AmadeusDestinationSelf {
+    self: string
+  }
+
+  interface AmadeusDestinationMeta {
+    origin: string
+    count: number
+    links: AmadeusDestinationSelf
+  }
+
+  interface AmadeusDestinationCity {
+    type: string
+    subtype: string
+    name: string
+    iataCode: string
+  }
+
+  interface AmadeusDestinationResult {
+    meta: AmadeusDestinationMeta
+    data: AmadeusDestinationCity[]
+  }
+
+  interface AmadeusDestinationErrorSource {
+    parameter: string
+    pointer: string
+    example: string
+  }
+
+  interface AmadeusDestinationError {
+    status: number
+    code: number
+    title: string
+    detail: string
+    source: AmadeusDestinationErrorSource
+  }
+}
