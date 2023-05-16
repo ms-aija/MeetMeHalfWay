f08b7f551e0356e37277c7d151651c6cd175b723
ms-aija <aija.avota@gmail.com>
2023-05-16 12:25:38 +0200

    feat: BE - airport search from API

--- server/controllers/destinations.ts
+++ server/controllers/destinations.ts
@@ -1,15 +1,6 @@
-// -- DESTINATIONS CONTROLLER
-// -- Destination data from Amadeus API
-import Amadeus from 'amadeus';
+import amadeus from '../util/AmadeusAPI';
 import { AmadeusDestinationResult, AmadeusDestinationError } from 'amadeus'
 import Express from 'express';
-
-
-const amadeus = new (Amadeus as any)({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  hostname: 'test'
-});
 
 export function getDestinationCityList(req: Express.Request, res: Express.Response) {
   amadeus.client.get('/v1/airport/direct-destinations', { departureAirportCode: req.params.id, max: 300 })
--- server/router.ts
+++ server/router.ts
@@ -2,12 +2,14 @@
 
 import { Router } from 'express';
 import { getAirportList } from './controllers/airports';
+import {  getAirports } from './controllers/airports';
 import { getDestinationCityList } from './controllers/destinations';
 
 const router = Router();
 
 // -- Routes
-router.get('/airports', getAirportList);
-router.get('/destinations/:id', getDestinationCityList);
+router.get('/airports/:airportName', getAirports); // gets n airports from Amadeus API
+// router.get('/airports', getAirportList); // gets all airpots from the json file
+router.get('/destinations/:id', getDestinationCityList); // gets all destinations for a given airport
 
 export default router;
