03d49c04bd4403085f42df464e911bc0bca28098
ms-aija <aija.avota@gmail.com>
2023-10-11 18:51:12 +0200

    feat: standardize success and error message format in the airportsService

--- client/src/services/airportsService.ts
+++ client/src/services/airportsService.ts
@@ -1,13 +1,22 @@
+import { IAirport } from '../interfaces/Airports';
 import { IDestinationCity } from '../interfaces/DestinationCities';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { airportSearchResultMock } from '../mocks/airportSearchResultMock';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { destinationResultMocks } from '../mocks/destinationResultMocks';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BASE_URL = 'http://localhost:3005';
 
-export const getAirportSearchData = async (searchTerm: string) => {
-  // ! RETURN ACTUAL DATA
+export const getAirportSearchData = async (
+  searchTerm: string
+): Promise<{
+  ok: boolean;
+  status: number;
+  body: IAirport[] | { code: number; message: string; description: string };
+}> => {
+  // ! RETURN ACTUAL DATA-------------------
   // let response;
-
   // try {
   //   response = await fetch(`${BASE_URL}/airports/${searchTerm}`);
   // } catch (err) {
@@ -23,14 +32,14 @@ export const getAirportSearchData = async (searchTerm: string) => {
   //     },
   //   };
   // }
-
   // return {
   //   ok: response.ok,
   //   status: response.status,
   //   body: await response.json(),
   // };
+  // ! -------------------------------------
 
-  // ! RETURN MOCK DATA
+  // ! RETURN MOCK DATA---------------------
   if (searchTerm === '') {
     return {
       ok: true,
@@ -50,24 +59,48 @@ export const getAirportSearchData = async (searchTerm: string) => {
       body: res,
     };
   }
+  // ! -------------------------------------
 };
 
-export const getDestinationCityList = (
+export const getDestinationCityList = async (
   iataCode: string
-): Promise<IDestinationCity[]> => {
-  // ! RETURN ACTUAL DATA
-  // return fetch(`${BASE_URL}/destinations/${iataCode}`)
-  //   .then((result) => {
-  //     const destResponse = result.json();
-  //     return destResponse;
-  //   })
-  //   .catch((err) => console.error(err));
+): Promise<{
+  ok: boolean;
+  status: number;
+  body:
+    | IDestinationCity[]
+    | { code: number; message: string; description: string };
+}> => {
+  // ! RETURN ACTUAL DATA-------------------
+  // let response;
+  // try {
+  //   response = await fetch(`${BASE_URL}/destinations/${iataCode}`);
+  // } catch (err) {
+  //   response = {
+  //     ok: false,
+  //     status: 500,
+  //     json: async () => {
+  //       return {
+  //         code: 500,
+  //         message: 'The server is not responding',
+  //         description: `${err}`,
+  //       };
+  //     },
+  //   };
+  // }
+  // return {
+  //   ok: response.ok,
+  //   status: response.status,
+  //   body: await response.json(),
+  // };
+  // ! -------------------------------------
 
-  // ! RETURN MOCK DATA
+  // ! RETURN MOCK DATA---------------------
   const randomIndex = Math.floor(Math.random() * 10);
   const subsetOfMocks = destinationResultMocks.slice(
     randomIndex,
     randomIndex + 20
   );
-  return Promise.resolve(subsetOfMocks);
+  return Promise.resolve({ ok: true, status: 200, body: subsetOfMocks });
+  // ! -------------------------------------
 };
